ca2c216de625b152c4310ca1d3cc5b4b5b69cfa3
Yisus95 <Yisus95@10.230.170.15>
2017-11-29 17:24:42 +0000

    Versión 3: Insertar emails en la tabla EMAIL

--- src/Kata5P1.java
+++ src/Kata5P1.java
@@ -1,19 +1,27 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Kata5P1{
     
     
-    public static void main(String args[]) throws ClassNotFoundException, SQLException{
+    public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException{
         Class.forName("org.sqlite.JDBC");
         createTableDB();
         consultaDb();
+        getAllEmails();
         
     }
 
@@ -34,6 +42,25 @@ public class Kata5P1{
                                                                         + "mail text NOT NULL);");
             ps.execute();
 
+        }
+    }
+
+    private static void getAllEmails() throws FileNotFoundException, IOException, SQLException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/emails.txt")));
+        String mail = "";
+        while((mail = reader.readLine()) != null){
+            if(mail.contains("@")){
+                insertIntoEmails(mail);
+            }
+        }
+        
+    }
+
+    private static void insertIntoEmails(String mail) throws SQLException {
+        try (Connection con = DriverManager.getConnection("jdbc:sqlite:/Users/Yisus95/NetBeansProjects/Bases de datos/ejemplo KATA5.db")) {
+            PreparedStatement ps= con.prepareStatement("insert into EMAIL (mail) values(?)");
+            ps.setString(1, mail);
+            ps.execute();
         }
     }
 
